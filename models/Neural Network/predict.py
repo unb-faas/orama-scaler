79554594cb6f54f2b549c74d5ca1d218f35eea1a
leonardoreboucas <leonardo_r_carvalho@yahoo.com.br>
2024-07-15 15:05:35 -0300

    Add

--- models/Neural Network/predict.py
+++ models/Neural Network/predict.py
@@ -19,7 +19,8 @@ model = load_model('model.keras', custom_objects={'r2_metric': r2_metric})
 data = pd.read_csv('to-predict.csv')
 
 # Supõe que o arquivo de predição não contém a coluna 'Latency'
-X = data
+X = data.drop(columns=['target'])
+y_true = data['target']
 
 # Normalizar os dados (importante para Redes Neurais)
 scaler = StandardScaler()
@@ -28,8 +29,24 @@ X = scaler.fit_transform(X)
 # Fazer previsões
 predictions = model.predict(X)
 
+
+data['Predicted_Latency'] = predictions
+data['target'] = y_true
+data['Difference'] = data['Predicted_Latency'] - data['target']
+data['Percentual_Diferenca'] = (abs(data['Difference']) / data['target']) * 100
+
+data = data.drop(columns=['success','total_operands','distinct_operands','total_operators','distinct_operators','time','bugs','effort','volume','difficulty','vocabulary','length'])
+
+
+somatorio = sum(data['Percentual_Diferenca'])
+print("Somatorio do percentual de diferenca: " + str(somatorio))
+
 # Se você quiser salvar as previsões em um arquivo CSV
-output = pd.DataFrame(predictions, columns=['Predicted_Latency'])
-output.to_csv('predictions.csv', index=False)
+#output = pd.DataFrame(predictions, columns=['Predicted_Latency'])
+#output.to_csv('predictions.csv', index=False)
+
+data.to_csv('predictions_with_data.csv', index=False)
+
+#data.to_csv('predictions_with_data.csv', index=False)
 
 print('Predictions saved to predictions.csv')
--- models/Neural Network/train.py
+++ models/Neural Network/train.py
@@ -3,19 +3,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+#from tensorflow.keras.layers import Dense
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 from tensorflow.keras.activations import swish
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.optimizers import SGD
-
+from tensorflow.keras.layers import SimpleRNN
+from tensorflow.keras.layers import LSTM, Dense, Dropout
 
 # 1. Carregar os Dados
 data = pd.read_csv('../../dataset/outputs/dataset.csv')
 
-pd.set_option('display.max_rows', 100)  # Número máximo de linhas a serem exibidas
-pd.set_option('display.max_columns', 20)  # Número máximo de colunas a serem exibidas
+#pd.set_option('display.max_rows', 100)  # Número máximo de linhas a serem exibidas
+#pd.set_option('display.max_columns', 20)  # Número máximo de colunas a serem exibidas
 
 
 label_encoder = LabelEncoder()
@@ -32,6 +33,44 @@ data['Latency'] = label_encoder.fit_transform(data['Latency'])
 #data = data.query('usecase_encoded == 2')
 #data = data.query('provider_encoded == 3')
 
+
+# calc afc 1 340.3
+# calc azf 1 1101.5
+# calc gcf 1 433.53333333333336
+# calc lambda 1 514.9
+# calc afc 2 353.15
+# calc azf 2 1075.8333333333333
+# calc gcf 2 465.3
+# calc lambda 2 770.0416666666666
+# calc afc 128 339.75296875
+# calc azf 128 3252.572265625
+# calc gcf 128 1187.63515625
+# calc lambda 128 1394.3358072916667
+# api4dbaas afc 1 nan
+# api4dbaas azf 1 1437.4
+# api4dbaas gcf 1 900.1
+# api4dbaas lambda 1 618.6
+# api4dbaas afc 2 nan
+# api4dbaas azf 2 1682.05
+# api4dbaas gcf 2 1150.0
+# api4dbaas lambda 2 594.55
+# api4dbaas afc 128 nan
+# api4dbaas azf 128 3573.32578125
+# api4dbaas gcf 128 1549.18359375
+# api4dbaas lambda 128 1525.29375
+# api4os afc 1 nan
+# api4os azf 1 941.0
+# api4os gcf 1 799.8
+# api4os lambda 1 715.4
+# api4os afc 2 nan
+# api4os azf 2 1046.1
+# api4os gcf 2 894.45
+# api4os lambda 2 782.85
+# api4os afc 128 nan
+# api4os azf 128 6792.68515625
+# api4os gcf 128 1917.28828125
+# api4os lambda 128 1481.1328125
+
 data_temp = data.drop(columns=['Latency', 'timeStamp', 'label', 'success','concurrency','total_operands','distinct_operands','total_operators','distinct_operators','time','bugs','effort','volume','difficulty','vocabulary','length'], axis=1)
 #mapping = dict(zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_)))
 # print(data_temp)
@@ -46,7 +85,7 @@ data_temp = data.drop(columns=['Latency', 'timeStamp', 'label', 'success','concu
 #             mean_latency = filtered_df['Latency'].mean()
 #             print(str(usecase) + ' ' + str(provider) + ' ' + str(concurrence) + ' ' + str(mean_latency))
 
-#exit()
+# exit()
 
 # 2. Pré-processamento
 X = data.drop(columns=['Latency', 'timeStamp', 'label', 'usecase', 'provider', 'usecase'], axis=1)
@@ -65,13 +104,60 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
+
 # 3. Construir o Modelo de Rede Neural
 model = Sequential()
-model.add(Dense(512, input_dim=X_train.shape[1], activation='relu'))
-model.add(Dense(256, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1))  # Saída de regressão (valor contínuo)
+#model.add(Dense(X_train.shape[1], activation='relu'))
+
+#model.add(Dense(64, input_dim=X_train.shape[1], activation='relu'))
+
+
+# model.add(Dense(56, activation='relu'))
+# model.add(Dense(48, activation='relu'))
+# model.add(Dense(40, activation='relu'))
+# model.add(Dense(32, activation='relu'))
+# model.add(Dense(24, activation='relu'))
+# model.add(Dense(16, activation='relu'))
+# model.add(Dense(8, activation='relu'))
+
+
+# Resultado bom!
+# Somatorio de deferencas: 2367.88 (5 ephocs)
+# model.add(Dense(7, activation='relu'))
+# model.add(Dense(3, activation='relu'))
+# model.add(Dense(7, activation='relu'))
+# model.add(Dense(15, activation='relu'))
+# model.add(Dense(7, activation='relu'))
+
+# Somatorio de deferencas: 2373.6976712799647
+# model.add(Dense(7, activation='relu'))
+# model.add(Dense(15, activation='relu'))
+# model.add(Dense(7, activation='relu'))
+# model.add(Dense(15, activation='relu'))
+# model.add(Dense(7, activation='relu'))
+
+# Somatorio de diferencas: 2735.059
+# model.add(Dense(X_train.shape[1], activation='relu'))
+# model.add(Dense(64, activation='relu'))
+# model.add(Dense(32, activation='relu'))
+# model.add(Dense(16, activation='relu'))
+
+# Somatorio de diferencas: 2563 - esquisitos (muitas predicoes 0.94)
+# model.add(Dense(7, activation='relu'))
+# model.add(Dense(15, activation='relu'))
+# model.add(Dense(7, activation='relu'))
+
+
+
+
+#model.add(Dense(1))  # Saída de regressão (valor contínuo)
+
+
+# Construir o modelo
+#model = Sequential()
+model.add(LSTM(units=50, activation='relu', input_shape=(X_train.shape[1], X_train.shape[2])))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
 
 # Função de métrica personalizada para R^2
 def r2_metric(y_true, y_pred):
@@ -89,7 +175,7 @@ model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mean_square
 #model.compile(optimizer='adam', loss='mean_squared_error', metrics=[r2_metric])
 
 # 4. Treinar o Modelo
-history = model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2, verbose=1)
+history = model.fit(X_train, y_train, epochs=5, batch_size=32, validation_split=0.2, verbose=1)
 
 model.save('model.keras')
 
